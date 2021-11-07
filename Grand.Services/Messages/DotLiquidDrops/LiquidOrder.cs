ebe3b7590bb4bace910dcb5780fe67a6df1ad28a
teodoraperisin <74027217+teodoraperisin@users.noreply.github.com>
2021-11-07 15:13:09 +0100

    S4487

--- Grand.Services/Messages/DotLiquidDrops/LiquidOrder.cs
+++ Grand.Services/Messages/DotLiquidDrops/LiquidOrder.cs
@@ -25,13 +25,12 @@ namespace Grand.Services.Messages.DotLiquidDrops
         private OrderNote _orderNote;
         private ICollection<LiquidOrderItem> _orderItems;
         private Vendor _vendor;
-        public LiquidOrder(Order order, Customer customer,Language language, Currency currency, Store store, OrderNote orderNote = null, Vendor vendor = null)
+        public LiquidOrder(Order order, Customer customer, Currency currency, Store store, OrderNote orderNote = null, Vendor vendor = null, Language language=null)
         {
 
             _order = order;
             _customer = customer;
-            _language = language
-                        ?? throw ArgumentNullException(nameof(language));
+            _language = language;
             _orderNote = orderNote;
             _currency = currency;
             _store = store;
