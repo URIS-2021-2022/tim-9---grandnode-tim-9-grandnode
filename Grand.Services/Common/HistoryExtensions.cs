c4c511c6daea89181d1fc436024adb83d8dfb2ff
tomanovicmilica <tomanovicmilica@uns.ac.rs>
2021-11-08 13:29:07 +0100

    S2326

--- Grand.Services/Common/HistoryExtensions.cs
+++ Grand.Services/Common/HistoryExtensions.cs
@@ -12,7 +12,7 @@ namespace Grand.Services.Common
         /// Save an entity
         /// </summary>
         /// <param name="entity">Entity</param>
-        public static async Task SaveHistory<T>(this BaseEntity entity, IHistoryService historyService) where T : BaseEntity, IHistory
+        public static async Task SaveHistory(this BaseEntity entity, IHistoryService historyService) 
         {
             if (entity == null)
                 throw new ArgumentNullException("entity");
