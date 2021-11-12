650dc0f9e0b2e8b533b838e71d12593f7041fa9d
Sharr0 <mgalic999@uns.ac.rs>
2021-11-12 03:09:25 +0100

    Revert "S1116"
    This reverts commit 24fef7e28962065790b7257402b7d0753465200b.

--- Grand.Services/Catalog/ProductAttributeParser.cs
+++ Grand.Services/Catalog/ProductAttributeParser.cs
@@ -347,10 +347,10 @@ namespace Grand.Services.Catalog
                 customAttributes = new List<CustomAttribute>();
 
             recipientName = customAttributes.FirstOrDefault(x => x.Key == "RecipientName")?.Value;
-            recipientEmail = customAttributes.FirstOrDefault(x => x.Key == "RecipientEmail")?.Value; 
-            senderName = customAttributes.FirstOrDefault(x => x.Key == "SenderName")?.Value; 
-            senderEmail = customAttributes.FirstOrDefault(x => x.Key == "SenderEmail")?.Value; 
-            giftCardMessage = customAttributes.FirstOrDefault(x => x.Key == "Message")?.Value; 
+            recipientEmail = customAttributes.FirstOrDefault(x => x.Key == "RecipientEmail")?.Value; ;
+            senderName = customAttributes.FirstOrDefault(x => x.Key == "SenderName")?.Value; ;
+            senderEmail = customAttributes.FirstOrDefault(x => x.Key == "SenderEmail")?.Value; ;
+            giftCardMessage = customAttributes.FirstOrDefault(x => x.Key == "Message")?.Value; ;
 
         }
 
